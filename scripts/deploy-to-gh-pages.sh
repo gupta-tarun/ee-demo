b64da92609e14146d4a22152cc9e056d09e0f93c
Tarun <gupta-tarun@users.noreply.github.com>
2016-05-03 21:17:55 +0530

    Update deploy-to-gh-pages.sh

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -13,5 +13,5 @@ echo 'added all files'
 git commit -m "Deploy to Github Pages"
 echo 'commit all files, pushing to gh-pages'
 
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages
 echo 'push is complete'
